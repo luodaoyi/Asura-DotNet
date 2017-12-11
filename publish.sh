8ea8ec53274a58fe7e3da229b8ab2538913c29ae
luodaoyi <luodaoyi@gmail.com>
2017-12-12 00:51:05 +0800

    增加自用的发布小脚本

---
+++ publish.sh
@@ -0,0 +1,11 @@
+echo "ssh 到服务器上停止服务！"
+ssh jd "systemctl stop asura-dev;exit"
+echo "进入项目目录准备编译发布"
+cd Asura/
+dotnet publish -c Release -o Publish --runtime ubuntu.16.04-x64
+echo "复制项目文件到远程服务器上"
+scp -r ~/RiderProjects/Asura/Asura/Publish jd:/var/www/asura
+echo "ssh 到服务器上重启服务！"
+ssh jd "systemctl restart asura-dev;exit"
+echo "删除发布文件夹，否则下次调试会失败"
+rm -rf ~/RiderProjects/Asura/Asura/Publish/
